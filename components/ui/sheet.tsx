fa3503c0d6dea23766e6f88d8491d1eedd29fb45
ace.vizserve <vercel.hfse@gmail.com>
2026-04-24 19:23:27 +0800

    feat(ui): Dialog/Sheet/AlertDialog gain inset ring highlight

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -28,7 +28,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  'fixed z-50 gap-4 bg-white p-6 shadow-[-8px_0_32px_-8px_rgba(15,23,42,0.2)] transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out',
+  'fixed z-50 gap-4 bg-white p-6 shadow-[-8px_0_32px_-8px_rgba(15,23,42,0.2)] ring-1 ring-inset ring-hairline transition ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out',
   {
     variants: {
       side: {
